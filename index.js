30595e2b3fa58d8ce1aa8d5281a695f5f9b9e9b5
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 21:50:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+//10.	Adding Comments: Choose two of the programs you’ve written,
+// and add at least one comment to each. If you don’t have anything 
+// specific to write because your programs are too simple at this point, 
+//just add your name and the current date at the top of each program file.
+// Then write one sentence describing what the program does.
+//This program does concatenation of statements using plus sign.
+console.log("My Name is Ajmal and" + " I am from Lahore." + " I am studying web 3.0");
+//This program performs addition process on two numbers.
+console.log(35 + 45);
